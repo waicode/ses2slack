7f124a4f631c04e7c847d8f4f47fc6665190a99c
waicode <waicode37@gmail.com>
2021-03-17 21:09:31 +0900

    feat: initial commit

--- serverless.ts
+++ serverless.ts
@@ -1,28 +1,37 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import hello from '@functions/hello';
+import hello from "@functions/hello";
 
 const serverlessConfiguration: AWS = {
-  service: 'ses2slack',
-  frameworkVersion: '2',
+  service: "ses2slack",
+  frameworkVersion: "2",
   custom: {
     webpack: {
-      webpackConfig: './webpack.config.js',
+      webpackConfig: "./webpack.config.js",
       includeModules: true,
     },
+    defaultStage: "local",
+    profile: "localstack",
+    localstack: {
+      debug: true,
+      stages: ["local"],
+      host: "http://localhost",
+      edgePort: 4566,
+      autostart: true,
+    },
   },
-  plugins: ['serverless-webpack'],
+  plugins: ["serverless-webpack", "serverless-localstack"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
     },
-    lambdaHashingVersion: '20201221',
+    lambdaHashingVersion: "20201221",
   },
   // import the function via paths
   functions: { hello },
