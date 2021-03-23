36c41d27a27d40c78f9240319232ac1c0b061c1f
waicode <waicode37@gmail.com>
2021-03-23 23:23:58 +0900

    feat: S3

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// const AWS = require("aws-sdk");
+const AWS = require("aws-sdk");
 const request = require("request");
 
 function generateJsonResponse(bodyJson, statusCode = 200) {
@@ -14,14 +14,33 @@ module.exports.mailToSlack = (event, context, callback) => {
   const sesData = event.Records[0].ses;
   console.log(sesData);
   const commonHeaders = sesData.mail.commonHeaders;
+  const messageId = sesData.mail.messageId;
 
   const subject = commonHeaders.subject;
+
+  const s3 = new AWS.S3();
+  const params = { Bucket: process.env.S3_MAIL_BUCKET_NAME, Key: messageId };
+  // s3.client.getObject(params)
+
+  // # Emlデータ取得
+  // raw_message = response['Body'].read()
 
   // TODO: S3に保存してから本文を取得する
   const messageBody = "てすと。てすと。てすと。\nてすと。てすと。てすと。";
 
   let messageText =
     `<!channel>\n*${subject}*\n\n` + "```" + `${messageBody}` + "```\n";
+
+  secret_name = process.env.SLACK_WEB_HOOK_SECRET;
+
+  secretsmanager_client = AWS.client(
+    "secretsmanager",
+    (region_name = process.env.AWS_REGION_NAME)
+  );
+  resp = secretsmanager_client.get_secret_value((SecretId = secret_name));
+  secret = json.loads(resp["SecretString"]);
+
+  SLACK_WEBHOOK_URL = secret["SLACK_WEBHOOK_URL"];
 
   const options = {
     url: process.env.SLACK_WEB_HOOK_URL,
